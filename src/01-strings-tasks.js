459ba1faf6026cf2a35048ccf7cc845c4cb363ee
Dmitriy An <pancake0890@mail.ru>
2020-12-07 20:06:01 +0500

    fix: fix line endings

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -325,11 +325,13 @@ function getCardId(value) {
     'A♥', '2♥', '3♥', '4♥', '5♥', '6♥', '7♥', '8♥', '9♥', '10♥', 'J♥', 'Q♥', 'K♥',
     'A♠', '2♠', '3♠', '4♠', '5♠', '6♠', '7♠', '8♠', '9♠', '10♠', 'J♠', 'Q♠', 'K♠'];
   let result;
+
   array.forEach((card, index) => {
     if (card === value) {
       result = index;
     }
   });
+
   return result;
 }
 
